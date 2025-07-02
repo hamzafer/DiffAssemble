1b26421ea37686e7cef500d2e669193c004e61fd
hamzafer <hamzafer3@gmail.com>
2025-07-03 00:59:51 +0200

    added cpu support for checking the eval while the model trains as it doesnt have early stopping.

--- commands-hamza.sh
+++ commands-hamza.sh
@@ -13,3 +13,19 @@ python puzzle_diff/train_script.py \
   --degree 100% \
   --backbone resnet18equiv \
   --architecture transformer
+
+# CPU
+CUDA_VISIBLE_DEVICES="" python puzzle_diff/train_script_cpu.py \
+    -dataset celeba \
+    -puzzle_sizes 6 8 10 12 \
+    -batch_size 2 \
+    -num_workers 2 \
+    -gpus 0 \
+    -steps 300 \
+    -inference_ratio 5 \
+    --rotation True \
+    --degree 100% \
+    --backbone resnet18equiv \
+    --architecture transformer \
+    --evaluate True \
+    --checkpoint_path "Puzzle-Diff/99qcofwy/checkpoints/last.ckpt"
