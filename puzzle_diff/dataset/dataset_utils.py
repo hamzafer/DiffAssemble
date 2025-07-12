42390b754c3753e44ecfe2cb9f52e485d1f71e15
hamzafer <hamzafer3@gmail.com>
2025-07-12 04:31:41 +0200

    ADDED RANDOM SCRAMBLE SHUFFLE

--- puzzle_diff/dataset/dataset_utils.py
+++ puzzle_diff/dataset/dataset_utils.py
@@ -98,6 +98,7 @@ def get_dataset(
         augment=augment,
         degree=degree,
         unique_graph=rng,
+        random=True,  # creating random puzzles
     )
     puzzleDt_test = Puzzle_Dataset(
         dataset=test_dt,
@@ -106,7 +107,7 @@ def get_dataset(
         augment=False,
         degree=-1 if inf_fully else degree,  # fully connected graph in inference
         unique_graph=rng,
-        random=False,  # creating random puzzles
+        random=True,  # creating random puzzles
     )
 
     return (puzzleDt_train, puzzleDt_test, real_puzzle_sizes)
